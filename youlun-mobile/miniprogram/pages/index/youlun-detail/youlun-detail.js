454c796fcf7eadfc98a4f395368141451989ccfa
CN\wangwujun <wangwujun@corp.netease.com>
2021-07-07 18:48:27 +0800

    add less

--- youlun-mobile/miniprogram/pages/index/youlun-detail/youlun-detail.js
+++ youlun-mobile/miniprogram/pages/index/youlun-detail/youlun-detail.js
@@ -6,11 +6,19 @@ Page({
   data: {
     options: {},
     swiperImageList: [
-      "../../static/images/home.png",
-      "../../static/images/home-dark.png",
-      "../../static/images/order.png",
+      "../../static/images/home.png"
     ],
-    introduction:"\t这里是测试文本\thello\n换行".repeat(100)
+    wayInfo:{
+      hot:true,
+      name: `上海-冲绳-八重山诸岛-上海-上海`,
+      youlun:`海洋光谱号`,
+      src: "https://resource.rcclchina.com.cn/000neo/1026/%E5%86%B2%E7%BB%B3-%E7%8E%BB%E7%92%83%E5%BA%95%E8%88%B9.jpg",
+      duration:"6天5晚",
+      start:"2022/02/07(周一)",
+      price:5096,
+      tags:['第二人4折','寒假','每舱再减'],
+      link:`./youlun-detail/youlun-detail?youlunId=0`,
+    },
   },
 
   onLoad: function (options) {
@@ -21,7 +29,7 @@ Page({
 
   onOrder:function(){
     wx.navigateTo({
-      url: `../room-list/room-list?youlunId=${this.data.options.youlunId}`,
+      url: `../room-list/room-list?wayId=${this.data.options.wayId}`,
     })
   }
 });
